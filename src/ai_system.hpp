500fcc6bbf7669bc8134954fd3861fe82ad9270f
UBC Student <hansooy@students.cs.ubc.ca>
2021-11-01 13:30:19 -0700

    Revert "MILESTONE 2 FINAL SUBMISSION"

--- src/ai_system.hpp
+++ src/ai_system.hpp
@@ -4,16 +4,9 @@
 
 #include "tiny_ecs_registry.hpp"
 #include "common.hpp"
-#include <random>
-
-extern std::vector<std::vector<int>> grid;
 
 class AISystem
 {
 public:
 	void step(float elapsed_ms);
-private:
-	void do_enemy_turn();
-	Turn decideMove(Entity& enemy, Entity& player);
-	std::vector<std::pair<int, int>> bfs(std::vector<std::vector<int>> grid, std::pair<int, int> curr, std::pair<int, int> goal);
 };
